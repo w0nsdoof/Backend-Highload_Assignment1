25734a7110e4d67459d98f1b9dfc2db7e9ef7b3f
Askar <w0nsdoof@gmail.com>
2024-09-03 20:29:33 +0500

    4. Admin Interface for Blog Posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # fields = ['title', 'content', 'author']
+    # exclude = ['created_at', 'updated_at']
+
+    list_display = ('title', 'author', 'created_at')
+
+    search_fields = ('title',)
+
+    date_hierarchy="created_at"
--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_created=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
