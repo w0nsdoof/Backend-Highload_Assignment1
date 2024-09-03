29a809bcf770a743b826d7ea2916ab02dd80620a
Askar <w0nsdoof@gmail.com>
2024-09-03 20:01:33 +0500

    3. Creating Blog Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
